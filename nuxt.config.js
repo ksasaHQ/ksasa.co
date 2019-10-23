5ba73eef80916635477b3ebcdccfea66487f639f
Felix Deiters <username@users.noreply.github.com>
2019-10-24 00:13:39 +0200

    Make dev server accessible from other devices

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,5 +53,9 @@ export default {
      ** You can extend webpack config here
      */
     extend(config, ctx) {}
+  },
+  server: {
+    port: 8000, // default: 3000
+    host: '0.0.0.0' // default: localhost
   }
 }
