47325dc8904d87d26e3aa1406e4899f554a07644
Felix Deiters <username@users.noreply.github.com>
2020-01-16 13:41:28 +0100

    Add close button component

---
+++ components/CloseBack.vue
@@ -0,0 +1,18 @@
+<template>
+  <a @click="$router.go(-1)">&times;</a>
+</template>
+<style scoped lang="scss">
+a {
+  font-size: 4rem;
+  font-weight: 200;
+  position: fixed;
+  right: 1rem;
+  top: 1rem;
+  z-index: 100;
+
+  :hover {
+    border-bottom: none !important;
+    box-shadow: none !important;
+  }
+}
+</style>
