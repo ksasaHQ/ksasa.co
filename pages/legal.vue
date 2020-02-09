e73f493cd0ea018ae23a592e1f58b99c2c5486ff
Felix Deiters <username@users.noreply.github.com>
2020-02-09 18:42:37 +0100

    Typos & more inclusive language

--- pages/legal.vue
+++ pages/legal.vue
@@ -14,11 +14,11 @@
 
       <h2>Hinweis gemäß Online-Streitbeilegungs-Verordnung</h2>
       <p>
-        Nach geltendem Recht sind wir verpflichtet, Verbraucher auf die Existenz
-        der Europäischen Online-Streitbeilegungs-Plattform hinzuweisen, die für
-        die Beilegung von Streitigkeiten genutzt werden kann, ohne dass ein
-        Gericht eingeschaltet werden muss. Für die Einrichtung der Plattform ist
-        die Europäische Kommission zuständig. Die Europäische
+        Nach geltendem Recht sind wir verpflichtet, Verbraucherinnen auf die
+        Existenz der Europäischen Online-Streitbeilegungs-Plattform hinzuweisen,
+        die für die Beilegung von Streitigkeiten genutzt werden kann, ohne dass
+        ein Gericht eingeschaltet werden muss. Für die Einrichtung der Plattform
+        ist die Europäische Kommission zuständig. Die Europäische
         Online-Streitbeilegungs-Plattform ist hier zu finden:
         <a href="http://ec.europa.eu/odr" target="_blank" rel="nofollow"
           >http://ec.europa.eu/odr</a
@@ -35,13 +35,14 @@
       <h2>Datenschutzerklärung</h2>
       <h3>Geltungsbereich</h3>
       <p>
-        Diese Datenschutzerklärung soll die Nutzer dieser Website gemäß
+        Diese Datenschutzerklärung soll die Nutzerinnen dieser Website gemäß
         Bundesdatenschutzgesetz und Telemediengesetz über die Art, den Umfang
         und den Zweck der Erhebung und Verwendung personenbezogener Daten durch
-        den Websitebetreiber (im Folgenden "wir", "uns", "unsere") informieren.
+        den Websitebetreiberinnen (im Folgenden "wir", "uns", "unsere")
+        informieren.
       </p>
       <p>
-        Wir nehen Datenschutz sehr ernst und behandeln personenbezogenen Daten
+        Wir nehmen Datenschutz sehr ernst und behandeln personenbezogenen Daten
         vertraulich und entsprechend der gesetzlichen Vorschriften.
       </p>
       <p>
@@ -52,18 +53,19 @@
 
       <h3>Zugriffsdaten</h3>
       <p>
-        Wir bzw. Seitenprovider erheben Daten über Zugriffe auf die Seite und
-        speichern diese als „Server-Logfiles“ ab. Folgende Daten werden so
+        Wir bzw. Seitenproviderinnen erheben Daten über Zugriffe auf die Seite
+        und speichern diese als „Server-Logfiles“ ab. Folgende Daten werden so
         protokolliert:
       </p>
       <ul>
-        <li>Besuchte Website</li>
+        <li>Besuchte Websites</li>
         <li>Uhrzeit zum Zeitpunkt des Zugriffes</li>
         <li>
           Menge der gesendeten Daten in Byte Quelle/Verweis, von welchem Du auf
           die Seite gelangt bist
         </li>
-        <li>Verwendeter Browser Verwendetes Betriebssystem</li>
+        <li>Verwendeter Browser</li>
+        <li>Verwendetes Betriebssystem</li>
         <li>Verwendete IP-Adresse</li>
       </ul>
       <p>
@@ -90,26 +92,26 @@
       <p>
         Als personenbezogene Daten gelten sämtliche Informationen, welche dazu
         dienen, Deine Person zu bestimmen und welche zu Dir zurückverfolgt
-        werden können – also beispielsweise Dein Name, Dein E-Mail-Adresse und
+        werden können – also beispielsweise Dein Name, Deine E-Mail-Adresse und
         Telefonnummer.
       </p>
       <h3>Benutzerkonto</h3>
       <p>
-        Um über dieses Angebot Bestellungen tätigen zu können, muss jeder Kunde
+        Um über dieses Angebot Bestellungen tätigen zu können, muss jede Kundin
         ein passwortgeschütztes Kundenkonto einrichten. Dieses beinhaltet eine
         Übersicht über getätigte Bestellungen und aktive Bestellvorgänge. Sofern
-        Du als Kunde den Onlineshop verlässt, wirst Du automatisch ausgeloggt.
+        Du als Kundin den Onlineshop verlässt, wirst Du automatisch ausgeloggt.
       </p>
       <p>
-        Wir übernimmt keine Haftung für Passwortmissbrauch, sofern dieser nicht
+        Wir übernehmen keine Haftung für Passwortmissbrauch, sofern dieser nicht
         von uns selbst verursacht wurde.
       </p>
       <h3>
         Bestellvorgang
       </h3>
       <p>
-        Sämtliche Daten, welche im Rahmen einer Bestellabwicklung von den Kunden
-        eingegeben werden, werden gespeichert. Dazu gehören:
+        Sämtliche Daten, welche im Rahmen einer Bestellabwicklung von den
+        Kundinnen eingegeben werden, werden gespeichert. Dazu gehören:
       </p>
       <ul>
         <li>
@@ -123,8 +125,8 @@
       </ul>
       <p>
         Jene Daten, welche zur Auslieferung oder Auftragsabwicklung zwingend
-        notwendig sind, werden an dritte Dienstleister weitergegeben. Sowie die
-        Aufbewahrung Deiner Daten nicht mehr erforderlich oder gesetzlich
+        notwendig sind, werden an dritte Dienstleisterinnen weitergegeben. Sowie
+        die Aufbewahrung Deiner Daten nicht mehr erforderlich oder gesetzlich
         geboten ist, werden diese gelöscht.
       </p>
       <h3>
@@ -134,7 +136,7 @@
         Um Deine und unsere Sicherheit zu gewährleisten, führen wir eine
         Bonitätsprüfung vor Abschluss eines Vertrags durch. Nebst einer Prüfung
         der vergangene Transaktionen mit uns und eventueller Änderungen Deiner
-        persönlichen Daten greifen wir ebenfalls auf dritte Dienstleister
+        persönlichen Daten greifen wir ebenfalls auf dritte Dienstleisterinnen
         zurück. Deine Personendaten (Name, Adresse, Geburtsdatum) werden im Zuge
         der Bonitätsprüfung an die Stripe, Inc., 510 Townsend Street, San
         Francisco, CA 94103, USA gesandt.
@@ -148,7 +150,7 @@
       </p>
       <h3>Recht auf Auskunft, Berichtigung und Löschung</h3>
       <p>
-        Als Nutzer erhälst Du auf Antrag kostenlose Auskunft darüber, welche
+        Als Nutzerin erhälst Du auf Antrag kostenlose Auskunft darüber, welche
         personenbezogenen Daten über Dich gespeichert wurden. Sofern Dein Wunsch
         nicht mit einer gesetzlichen Pflicht zur Aufbewahrung von Daten (z. B.
         Vorratsdatenspeicherung) kollidiert, hast Du ein Anrecht auf
